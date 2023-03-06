fd738e4edc774e4ba26f784538588b01d720909b
Joaquin Feola <joacoarielfeola@gmail.com>
2023-03-06 11:25:42 -0300

    agregue la carpeta dist

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,41 @@
+// imports
 import React from 'react';
 import './Navbar.css';
+
+
+
 
 const OpenPlans = ( e ) => {
     e.preventDefault();
     document.getElementById('plans_content').classList.toggle('active');
+
 };
+const OpenNavLinks = ( e ) => {
+    const hamburguerBtnContainer = document.querySelector('.hamburguer-btn-container');
+    e.target.closest('.hamburguer-btn').classList.toggle('active'); 
+
+    document.querySelector('.nav-links').classList.toggle('active');
+
+    hamburguerBtnContainer.classList.toggle('active');
+
+    if ( hamburguerBtnContainer.classList.contains('active'))
+    {
+        document.querySelector('.nav').style.padding = '0px';
+    }
+    else
+    {
+        document.querySelector('.nav').style.padding = '5px'
+    };
+};
+
+// functional component
 
 export function Navbar() {
   return (
         <>
             <nav className='nav'>
                 <div className="hamburguer-btn-container">
-                    <button className="hamburguer-btn">
+                    <button className="hamburguer-btn" onClick={OpenNavLinks}>
                         <div className="line"></div>
                         <div className="line"></div>
                         <div className="line"></div>
@@ -19,7 +43,10 @@ export function Navbar() {
                 </div>
 
                 <div className="nav-links">
-                    <a href="#" className="nav-link">Inicio</a>
+                    <a href="#" className="nav-link">
+                    <i className="bi bi-house"></i>   
+                        Inicio
+                    </a>
                     <div className="plans-container">
                         <a href="#" className="nav-link plans-btn" onClick={OpenPlans}>Planes</a>
                         <div className="plans-content" id='plans_content'>
