366fbc3b3d8756d1db90c2ea63b79d37cce83657
Joaquin Feola <joacoarielfeola@gmail.com>
2023-03-06 00:24:09 -0300

    agregue los estilos al navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import './Navbar.css';
+
+const OpenPlans = ( e ) => {
+    e.preventDefault();
+    document.getElementById('plans_content').classList.toggle('active');
+};
+
 export function Navbar() {
   return (
         <>
             <nav className='nav'>
+                <div className="hamburguer-btn-container">
+                    <button className="hamburguer-btn">
+                        <div className="line"></div>
+                        <div className="line"></div>
+                        <div className="line"></div>
+                    </button>
+                </div>
+
                 <div className="nav-links">
                     <a href="#" className="nav-link">Inicio</a>
-                    <a href="#" className="nav-link plans-btn">
-                        Planes
-                        <div className="plans-content">
+                    <div className="plans-container">
+                        <a href="#" className="nav-link plans-btn" onClick={OpenPlans}>Planes</a>
+                        <div className="plans-content" id='plans_content'>
                             <a href="#" className="plan-link">Simple</a>
                             <a href="#" className="plan-link">Avanzado</a>
                         </div>
-                    </a>
+                    </div>
                     <a href="#" className="nav-link">Sobre nosotros</a>
                 </div>
             </nav>
